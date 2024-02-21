df1a967ed3ae204b45f96411d74bd09cf853c5e2
KobayashiRui <roboroborx782@gmail.com>
2024-02-22 05:34:10 +0900

    add rc-gantt-test1

---
+++ gantt-chart-example/app/rc-gantt-test1/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React, {useState} from "react"
+
+export default function RcGanttTest1() {
+  return (
+    <div className="flex h-screen flex-col m-8">
+      <label>RC-GANTT</label>
+    </div>
+  )
+}
